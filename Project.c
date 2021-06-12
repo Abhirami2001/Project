cbfff0ff12b3dc3f8a56a513d0bc5e092991c389
Abhirami2001 <77285161+Abhirami2001@users.noreply.github.com>
2021-06-12 18:43:57 +0530

    Create Project.c

---
+++ Project.c
@@ -0,0 +1,43 @@
+Digital clock
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int h, m, s;
+  int d=1000;
+  printf("set time:\n");
+  scanf("%d%d%d",&h,&m,&s);
+  if(h>12||m>60||s>60)
+  {
+    printf("error! \n");
+    exit(0);
+  }
+  while (1)
+  {
+    s++;
+    if(s>59)
+    {
+      m++;
+      s=0;
+    }
+    if(m>59)
+    {
+      h++;
+      m=0;
+    }
+    if(h>12)
+    {
+      h=1;
+    }
+    printf("\n clock:");
+    printf("\n%0.2d:%0.2d:%0.2d",h,m,s);
+    sleep(d);
+    system ("cls");
+    
+   
+  }
+  
+}
